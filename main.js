bfb4caef0c4c46a2717564dbc20bfa194ec23c68
Haidar Ali <shehrihaidarali@gmail.com>
2023-09-14 16:19:13 -0400

    feat: buttons work and erase and draw

--- main.js
+++ main.js
@@ -16,13 +16,48 @@ function initalGrid (numRows) {
         }
         return null;
 }
-initalGrid(16);
 
-const squares = document.querySelectorAll('.square');
+let userInput = prompt("How many squares x squares do you want? Make sure that you input a number between 1 and 100.");
 
-squares.forEach((square) => {
 
-        square.addEventListener('mouseover', () => {
-                square.style.backgroundColor = 'white';
+
+while ((Number.isInteger(parseInt(userInput)) == false) || (parseInt(userInput) > 100) || (parseInt(userInput) < 1)) {
+        userInput = prompt("How many squares x squares do you want? Make sure that you input a number between 1 and 100.");
+        
+}
+
+window.onload=function () {
+        initalGrid(userInput);
+
+
+        const whiteButton = document.querySelector('.whiteButton');
+        const eraserButton = document.querySelector('.eraserButton');
+
+        console.log(whiteButton);
+        console.log(eraserButton);
+
+        whiteButton.addEventListener('click', () => {
+                whiteButton.classList.add('clicked');
+                eraserButton.classList.remove('clicked');
         });
-});
+
+        eraserButton.addEventListener('click', () => {
+                eraserButton.classList.add('clicked');
+                whiteButton.classList.remove('clicked');
+        })
+
+
+        const squares = document.querySelectorAll('.square');
+
+        squares.forEach((square) => {
+                square.addEventListener('mouseover', () => {
+                        console.log('1');
+                        if (whiteButton.classList.contains('clicked')) {
+                                console.log('2');
+                                square.style.backgroundColor = 'white';
+                        } else if (eraserButton.classList.contains('clicked')) {
+                                square.style.backgroundColor = 'black';
+                        }
+                });
+        });
+}
